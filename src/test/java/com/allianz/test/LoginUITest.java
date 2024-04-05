a3aa2c363c0f1bb18868394d6427ce5d6a99a8f6
shibinparayi <shibinp@gmail.com>
2024-04-05 10:29:40 -0700

    hybrid framework frist commit

---
+++ src/test/java/com/allianz/test/LoginUITest.java
@@ -0,0 +1,17 @@
+package com.allianz.test;
+
+import org.testng.annotations.Test;
+
+public class LoginUITest {
+	
+	@Test
+	public void titleTest() {
+		System.out.println("title test");
+	}
+	
+	@Test
+	public void appLoginDecriptionTest() {
+		System.out.println("description test");
+	}
+
+}
