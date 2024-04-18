f3cb84acacac260db07698ca7671f08cc2923244
shibinparayi <shibinp@gmail.com>
2024-04-17 23:08:25 -0700

    framework update

---
+++ src/test/java/com/allianz/test/LoginTest.java
@@ -0,0 +1,30 @@
+package com.allianz.test;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.allianz.base.AutomationWrapper;
+import com.allianz.utils.DataUtils;
+
+public class LoginTest extends AutomationWrapper {
+	@Test
+	public void validLoginTest() {
+		driver.findElement(By.name("username")).sendKeys("Admin");
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+
+		String actualHeader = driver.findElement(By.xpath("//h6[contains(normalize-space(),'Dash')]")).getText();
+		Assert.assertEquals(actualHeader, "Dashboard");
+	}
+
+	@Test(dataProvider = "invalidLoginData",dataProviderClass = DataUtils.class)
+	public void invalidLoginTest(String username, String password, String expectedError) {
+		driver.findElement(By.name("username")).sendKeys(username);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+
+		String actualError = driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid')]")).getText();
+		Assert.assertEquals(actualError, expectedError);
+	}
+}
--- src/test/java/com/allianz/test/LoginUITest.java
+++ src/test/java/com/allianz/test/LoginUITest.java
@@ -1,17 +1,25 @@
 package com.allianz.test;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class LoginUITest {
+import com.allianz.base.AutomationWrapper;
+
+public class LoginUITest extends AutomationWrapper {
 	
 	@Test
-	public void titleTest() {
-		System.out.println("title test");
+	public void titleTest()
+	{
+		String actualTitle=driver.getTitle();
+		Assert.assertEquals(actualTitle, "OrangeHRM");
 	}
 	
 	@Test
-	public void appLoginDecriptionTest() {
-		System.out.println("description test");
+	public void applicationDescriptionTest()
+	{
+		String actualVersion=driver.findElement(By.xpath("//p[contains(normalize-space(),'OS')]")).getText();
+		Assert.assertEquals(actualVersion, "OrangeHRM OS 5.6.1");
 	}
 
 }
