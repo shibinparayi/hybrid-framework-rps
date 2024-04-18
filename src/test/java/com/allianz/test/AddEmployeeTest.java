6075ef1c13d612e4f70019e5119c451e4acdea36
shibinparayi <shibinp@gmail.com>
2024-04-17 23:28:38 -0700

    framework update 2

--- src/test/java/com/allianz/test/AddEmployeeTest.java
+++ src/test/java/com/allianz/test/AddEmployeeTest.java
@@ -2,6 +2,8 @@ package com.allianz.test;
 
 import static org.testng.Assert.assertEquals;
 
+import java.io.File;
+
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -9,25 +11,31 @@ import org.testng.annotations.Test;
 import com.allianz.base.AutomationWrapper;
 
 public class AddEmployeeTest extends AutomationWrapper {
-	
+
 	@Test
-	public void uploadInvalidDisplayPictureTest()
-	{
+	public void uploadInvalidDisplayPictureTest() {
+
+		File file = new File("src/test/resources/files/Path.txt");
+		String path = file.getAbsolutePath();
+		System.out.println(path);
+
 		driver.findElement(By.name("username")).sendKeys("Admin");
 		driver.findElement(By.name("password")).sendKeys("admin123");
 		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
-		
+
 		driver.findElement(By.xpath("//span[text()='PIM']")).click();
 		driver.findElement(By.linkText("Add Employee")).click();
-		driver.findElement(By.xpath("//input[@type='file']")).sendKeys("C:\\Automation Session\\automation_workspace\\HealthManagementAutomation\\pom.xml");
-		
+		// driver.findElement(By.xpath("//input[@type='file']")).sendKeys("C:\\Automation
+		// Session\\automation_workspace\\HealthManagementAutomation\\pom.xml");
+		driver.findElement(By.xpath("//input[@type='file']")).sendKeys(path);
+
 		String actualError = driver.findElement(By.xpath("//span[contains(normalize-space(),'File type')]")).getText();
 		Assert.assertTrue(actualError.contains("File type not allowed"));
-				
-		//click on PIM menu
-		//click on add employee
-		//upload the pdf file instead of image
-		//Assert the error - File type not allowed
+
+		// click on PIM menu
+		// click on add employee
+		// upload the pdf file instead of image
+		// Assert the error - File type not allowed
 	}
 
 }
